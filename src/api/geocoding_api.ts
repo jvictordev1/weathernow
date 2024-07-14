8f8bb5328ae94b18d1b37b21d51d5a234299ed59
jvictordev1 <josevictorcruzrb@gmail.com>
2024-07-14 19:12:51 -0300

    fix: fixing mixed content errors when deploying

--- src/api/geocoding_api.ts
+++ src/api/geocoding_api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const geoInstance = axios.create({
-  baseURL: "http://api.openweathermap.org/geo/1.0/",
+  baseURL: "https://api.openweathermap.org/geo/1.0/",
 });
 
 export default geoInstance;
