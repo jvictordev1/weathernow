d9cc23651c44fc65b8d16bbd4ae33bc9ee93545a
jvictordev1 <josevictorcruzrb@gmail.com>
2024-07-14 20:53:38 -0300

    change: changing footer style

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,12 +5,9 @@ export default function Footer() {
     <>
       <Divisor />
       <footer className="w-full font-medium flex items-center justify-center gap-2 py-2 mb-4">
-        <h2 className="text-lg">Powered by</h2>
-        <img
-          className="w-20"
-          src="../src/assets/openweatherlogo.png"
-          alt="OpenWeather logo"
-        />
+        <h2 className="text-xs sm:text-xl">
+          Powered by OpenWeather and Open-Meteo.
+        </h2>
       </footer>
     </>
   );
